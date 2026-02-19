ced7e79e5dd7ace261ea56b29c373b20ee852394
leviszz <levidias2007@gmail.com>
2026-02-19 18:58:23 -0300

    Correção de bug Vercel

--- src/pages/forgotPassword.jsx
+++ src/pages/forgotPassword.jsx
@@ -14,7 +14,7 @@ export default function ForgotPassword() {
     setMsg("") // Limpa mensagens antigas
 
     try {
-      const response = await fetch("http://localhost:8000/auth/forgot-password", {
+      const response = await fetch("https://racker-ultra-api-update.onrender.com/auth/forgot-password", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
