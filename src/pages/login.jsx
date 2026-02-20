ca754e3f45f8501faaef5218349ca3a164ff72b5
leviszz <levidias2007@gmail.com>
2026-02-19 22:14:42 -0300

    Correção de bug fetch

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     setLoading(true)
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/auth/jwt/login", {
+      const response = await fetch("https://racker-ultra-api-update.onrender.com/auth/jwt/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
